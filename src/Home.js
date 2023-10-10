5f5596b3a85dcb3954911525e248a8abd787a919
Eylül Naz Can <eylulnc@gmail.com>
2023-10-10 12:30:22 +0200

    Until Tutorial Video 7

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("hello ninjas", e );
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("hello " + name);
+    }
+
+
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain("Mario")}>Click again</button>
+        </div>
+     );
+}
+ 
+export default Home;
